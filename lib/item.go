a185a2f0de5f21f9665c4153accb984fd2b89ead
mono <mono@mono0x.net>
2016-11-17 16:58:26 +0900

    Remove Location

--- lib/item.go
+++ lib/item.go
@@ -1,12 +1,6 @@
 package murmur
 
-type Location struct {
-	Latitude  float64
-	Longitude float64
-}
-
 type Item struct {
-	Summary  string
-	Url      string
-	Location *Location
+	Summary string
+	Url     string
 }
